37fbeaa4fca0780b3bfab4c36b57bf040a833b17
kh4ydarov <kh4ydarovs@yandex.ru>
2024-03-18 09:48:38 +0500

    Исправленны ошибки согласно комментарии.

--- conftest.py
+++ conftest.py
@@ -1,6 +1,4 @@
 import pytest
-
-
 from selenium import webdriver
 from selene import browser, be, have
 
@@ -8,9 +6,6 @@ from selene import browser, be, have
 @pytest.fixture(autouse=True)
 def config_browser_window():
     browser.open()
-    browser.config.window_size = (1920, 1080)
-    browser.config.start_maximized = True
-    browser.open('https://google.com')
     yield # dalshe teardown
     print('Закрываем браузер')
     browser.quit()
--- test_google_homework3.py
+++ test_google_homework3.py
@@ -1,16 +1,19 @@
 import pytest
-
-
-from selenium import webdriver
 from selene import browser, be, have
 
 
-def test_google():
+@pytest.fixture()
+def open_browser():
+    browser.driver.set_window_size(1920, 1080)
+    browser.open('https://google.com')
+
+
+def test_google(open_browser):
     browser.element('[name="q"]').should(be.blank).type('yashaka/selene').press_enter()
     browser.element('[id="search"]').should(have.text('Selene - User-oriented Web UI browser tests in Python'))
 
 
-def test_negative():
+def test_negative(open_browser):
     browser.element('[name="q"]').should(be.blank).type('diudihfdsjfgfdg').press_enter()
     browser.element('[id="botstuff"]').should(
         have.text('diudihfdsjfgfdg ничего не найдено.'))
